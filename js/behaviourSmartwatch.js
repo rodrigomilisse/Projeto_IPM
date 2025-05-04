249ac97459c88c8f47f0f327cc5a99ae68d7c7dc
rodrigomilisse <rodrigomilisse@gmail.com>
2025-05-04 16:57:18 +0100

    .

--- js/behaviourSmartwatch.js
+++ js/behaviourSmartwatch.js
@@ -1,22 +1,22 @@
 function mudarEstado(estado) {
-    const botaoAtivar = document.getElementById("ativar");
-    const botaoDesativar = document.getElementById("desativar");
+	const botaoAtivar = document.getElementById("ativar");
+	const botaoDesativar = document.getElementById("desativar");
 
-    if (estado === 'ativar') {
-        botaoAtivar.classList.add("ativado");
-        botaoDesativar.classList.remove("ativado");
-    } else {
-        botaoDesativar.classList.add("ativado");
-        botaoAtivar.classList.remove("ativado");
-    }
+	if (estado === 'ativar') {
+		botaoAtivar.classList.add("ativado");
+		botaoDesativar.classList.remove("ativado");
+	} else {
+		botaoDesativar.classList.add("ativado");
+		botaoAtivar.classList.remove("ativado");
+	}
 }
 
 function confirmarAcao() {
-    alert("Alterações confirmadas!");
-    location.reload();
+	alert("Alterações confirmadas!");
+	location.reload();
 }
-  
-  function cancelarAcao() {
-    alert("Alterações canceladas!");
-    location.reload();
+
+function cancelarAcao() {
+	alert("Alterações canceladas!");
+	location.reload();
 }
