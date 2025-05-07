9fbd2547c3a450293462614e11bb4b467a82d208
rodrigomilisse <rodrigomilisse@gmail.com>
2025-05-07 18:01:42 +0100

    darkmode contrast fix

--- js/settings.js
+++ js/settings.js
@@ -10,11 +10,13 @@ function loadDarkMode() {
 		root.style.setProperty('--nav-bar-color', 'black');
 		root.style.setProperty('--menu-color', 'grey');
 		root.style.setProperty('--container-shadow', '0 2px 6px rgba(255, 255, 255, 0.5)');
+		root.style.setProperty('--value-color', 'white');
 	} else {
 		root.style.setProperty('--main-background', '#2A9D8F');
 		root.style.setProperty('--nav-bar-color', '#264653');
 		root.style.setProperty('--menu-color', '#F4F4F4');
 		root.style.setProperty('--container-shadow', '0 2px 6px rgba(0, 0, 0, 1)');
+		root.style.setProperty('--value-color', '#1E6091');
 	}
 }
 
